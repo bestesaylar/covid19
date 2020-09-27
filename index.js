e0f95ba683fabd2bf02054a276279b26cbda6360
bestesaylar <bestesaylar@gmail.com>
2020-09-27 18:31:30 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// const searchFrom=document.querySelector('.search');
+// const input=document.querySelector('.input');
+// const newsList=document.querySelector('.news-list');
+
+// searchFrom.addEventListener('submit', retrieve);
+
+// function retrieve(e){
+
+//     e.preventDefault();
+
+//     const apiKey = '58b9f311860c4667b64aed8c92f75ebc'
+
+//     let url=`https://newsapi.org/v2/everything?q=${topic}&apiKey=${apiKey}`
+
+//     fetch(url).then((res)=>{
+//         return res.json()
+//     }).then((data)=>{
+//         console.log(data)
+//     })
+
+//     let topic=input.value;
+
+//     console.log(topic);
+
+// }
+
+
